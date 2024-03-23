b02d34836cd70555deb7d672d803ccfffb6c3aa1
Abinav <abinavananda@gmail.com>
2024-03-23 15:41:08 +0530

    first_commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("btn2").addEventListener("click", function() {
+    document.getElementById("btn1").innerHTML = "New Content";
+  });
+  var button = document.getElementById("btn2");
+button.style.backgroundColor = "red"; 
+button.style.border = "5px solid black"; 
